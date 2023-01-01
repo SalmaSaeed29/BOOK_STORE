262bf74554c590361da1a2dfee85612e1799a2e4
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2023-01-01 10:20:48 +0200

    Add files via upload

---
+++ book/src/book/JFrame.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book;
+
+/**
+ *
+ * @author user
+ */
+class JFrame {
+
+    static int EXIT_ON_CLOSE;
+    
+}
